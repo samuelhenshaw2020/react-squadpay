7620727f0a39d7b8be3a523f98bf6313d59fdf9a
samuelhenshaw2020 <samuelhenshaw2020@gmail.com>
2022-10-28 18:55:57 +0100

    chores: started the gtco squad lib

---
+++ README.md
@@ -0,0 +1,1 @@
+# GTCO Squad Payment Gateway
---
+++ vite.config.ts
@@ -0,0 +1,47 @@
+import {resolve} from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import dts from "vite-plugin-dts"
+
+// https://vitejs.dev/config/
+export default defineConfig({  
+  publicDir: resolve(__dirname, "example", "public"),
+  root: resolve(__dirname, 'example'),
+  server: {
+    port: 3000
+  },
+  plugins: [
+    react({}), 
+    dts({
+      entryRoot: resolve(__dirname, "libs"),
+    })
+  ],
+  resolve: {
+    alias: {
+      '@/': new URL('./example/', import.meta.url).pathname
+    }
+  },
+  build:{
+    minify: "esbuild",
+    target: "esnext",
+    lib: {
+      entry: resolve(__dirname, "libs/index.ts"),
+      name: "react-gtco-squad",
+      fileName: (format) => `squad.${format}.js`,
+      formats: ['umd','es'],
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+
+      external: ['react'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  }
+})
