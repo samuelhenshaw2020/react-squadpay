eec6d7c18dc10f0feb3b067a50287d00451ec184
samuelhenshaw2020 <samuelhenshaw2020@gmail.com>
2022-10-29 06:57:34 +0100

    docs: readme

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import dts from "vite-plugin-dts"
 // https://vitejs.dev/config/
 export default defineConfig({  
   publicDir: resolve(__dirname, "example", "public"),
-  root: resolve(__dirname, 'example'),
+  // root: resolve(__dirname, 'example'),
   server: {
     port: 3000
   },
@@ -26,14 +26,13 @@ export default defineConfig({
     target: "esnext",
     lib: {
       entry: resolve(__dirname, "libs/index.ts"),
-      name: "react-gtco-squad",
+      name: "react-squadpay",
       fileName: (format) => `squad.${format}.js`,
       formats: ['umd','es'],
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-
       external: ['react'],
       output: {
         // Provide global variables to use in the UMD build
@@ -43,5 +42,6 @@ export default defineConfig({
         },
       },
     },
+    
   }
 })
